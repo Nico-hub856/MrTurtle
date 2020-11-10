73fb53817f8559a892959b4fe4634f4f30b82a8a
NicoNitro <60285611+Nico-hub856@users.noreply.github.com>
2020-11-10 21:49:14 +0100

    Update setup.py

--- Turtle v1.3/setup.py
+++ Turtle v1.3/setup.py
@@ -1,14 +1,14 @@
 import setuptools
 
 setuptools.setup(
-    name="Turtle",
+    name="MrTurtle",
     version="v1.3",
     author="Nicolas Elger",
     author_email="nicolas.elger@free.fr",
     description="A turtle made with pygame",
-    long_description="Controls:\nSPACE to show/hide the turtle, F1 key to zoom-in, F2 key to zoom-out, ARROWS to move arround, ESCAPE key or closing button to close the window.",
+    long_description="Turtle made with pygame. Controls:\nSPACE to show/hide the turtle, F1 key to zoom-in, F2 key to zoom-out, ARROWS to move arround, ESCAPE key or closing button to close the window.",
     url="https://github.com/Nico-hub856/Turtle",
     packages=setuptools.find_packages(exclude=("tests")),
     classifiers=["Programming Language :: Python :: 3"],
-    python_requires='>=3.6'
+    python_requires=">=3.6"
 )
