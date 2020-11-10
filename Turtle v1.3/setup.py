4625a181161baff6daf58ba68a2d6c6fd63e8003
NicoNitro <60285611+Nico-hub856@users.noreply.github.com>
2020-11-10 18:45:52 +0100

    Add files via upload

---
+++ Turtle v1.3/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+setuptools.setup(
+    name="Turtle",
+    version="v1.3",
+    author="Nicolas Elger",
+    author_email="nicolas.elger@free.fr",
+    description="A turtle made with pygame",
+    long_description="Controls:\nSPACE to show/hide the turtle, F1 key to zoom-in, F2 key to zoom-out, ARROWS to move arround, ESCAPE key or closing button to close the window."
+    url="https://github.com/Nico-hub/sampleproject",
+    packages=setuptools.find_packages(exclude=["tests"]),
+    classifiers=["Programming Language :: Python :: 3"],
+    python_requires='3.6'
+)
